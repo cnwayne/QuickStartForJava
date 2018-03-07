fad59db0b2afd240495f0dc0b8092253a1907ed7
wayne <wei@wayneleo.com>
2018-03-07 14:12:28 +0800

    update

--- quickstart-service/quickstart-service-sample/quickstart-service-sample-mvc/quickstart-service-sample-mvc-impl/src/main/java/com/wayneleo/quickstart/services/sample/mvc/SampleController.java
+++ quickstart-service/quickstart-service-sample/quickstart-service-sample-mvc/quickstart-service-sample-mvc-impl/src/main/java/com/wayneleo/quickstart/services/sample/mvc/SampleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.wayneleo.quickstart.framework.base.BaseController;
-import com.wayneleo.quickstart.services.sample.mvc.SampleResp;
+import com.wayneleo.quickstart.framework.base.BaseResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -16,6 +16,24 @@ import io.swagger.annotations.ApiOperation;
 @RestController( "W_SampleController" )
 @RequestMapping( value = "/api/v1/sample" )
 public class SampleController extends BaseController {
+    @ApiOperation( value = "测试PUT", notes = "返回正常结果" )
+    @RequestMapping( method = RequestMethod.PUT )
+    public BaseResponse testPut() {
+        System.out.println( "\n====== PUT" );
+        BaseResponse resp = new BaseResponse();
+        resp.setCode( 0 );
+        return resp;
+    }
+    
+    @ApiOperation( value = "测试Del", notes = "返回正常结果" )
+    @RequestMapping( method = RequestMethod.DELETE )
+    public BaseResponse testDel() {
+        System.out.println( "\n====== DEL" );
+        BaseResponse resp = new BaseResponse();
+        resp.setCode( 0 );
+        return resp;
+    }
+
     @ApiOperation( value = "接口01", notes = "返回正常结果" )
     @ApiImplicitParams( {
             @ApiImplicitParam( paramType = "query", name = "name", value = "用户名", required = true ),
