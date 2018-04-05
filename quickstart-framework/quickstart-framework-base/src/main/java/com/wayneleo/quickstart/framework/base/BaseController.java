fc952bdad564fa4a9f224edec1ed548013be9c4a
wayne <wei@wayneleo.com>
2018-04-05 14:52:41 +0800

    丰富控制层基础类

--- quickstart-framework/quickstart-framework-base/src/main/java/com/wayneleo/quickstart/framework/base/BaseController.java
+++ quickstart-framework/quickstart-framework-base/src/main/java/com/wayneleo/quickstart/framework/base/BaseController.java
@@ -1,7 +1,17 @@
 package com.wayneleo.quickstart.framework.base;
 
+import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+/**
+ * <pre>
+ * 基础Controller类型
+ * 实现部分公用方法
+ * </pre>
+ * 
+ * @author wayne
+ */
 public abstract class BaseController {
     /**
      * 获取被访问路径
@@ -24,5 +34,53 @@ public abstract class BaseController {
             ex.printStackTrace();
         }
         return url.toString();
+    }
+
+    /**
+     * 关闭窗口
+     */
+    protected void closeWindow( HttpServletResponse response ) {
+        StringBuffer html = new StringBuffer();
+        html.append( "<script type=\"text/javascript\">" );
+        html.append( "window.opener=null;" );
+        html.append( "window.open('', '_self', '');" );
+        html.append( "window.close();" );
+        html.append( "</script>" );
+        this.renderHtml( html.toString(), response );
+    }
+
+    /**
+     * 直接输出字符串.
+     */
+    protected String renderText( String text, HttpServletResponse response ) {
+        return render( text, "text/plain;charset=UTF-8", response );
+    }
+
+    /**
+     * 直接输出HTML.
+     */
+    protected String renderHtml( String html, HttpServletResponse response ) {
+        return render( html, "text/html;charset=UTF-8", response );
+    }
+
+    /**
+     * 直接输出XML.
+     */
+    protected String renderXML( String xml, HttpServletResponse response ) {
+        return render( xml, "text/xml;charset=UTF-8", response );
+    }
+
+    /**
+     * 直接输出文本.
+     */
+    protected String render( String text, String contentType, HttpServletResponse response ) {
+        try {
+            response.setContentType( contentType );
+            response.getWriter().write( text );
+        }
+        catch ( IOException e ) {
+            throw new BaseException( 10001, e );
+        }
+        return null;
     }
 }
