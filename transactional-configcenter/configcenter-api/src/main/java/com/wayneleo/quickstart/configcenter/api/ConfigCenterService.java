8d71cbce6887772e52b7fb3e1d452acacf2b0127
wayne <wei@wayneleo.com>
2019-01-09 14:43:44 +0800

    定义向外部暴露的接口

--- transactional-configcenter/configcenter-api/src/main/java/com/wayneleo/quickstart/configcenter/api/ConfigCenterService.java
+++ transactional-configcenter/configcenter-api/src/main/java/com/wayneleo/quickstart/configcenter/api/ConfigCenterService.java
@@ -1,7 +1,28 @@
 package com.wayneleo.quickstart.configcenter.api;
 
+import java.util.List;
+import java.util.Map;
 import com.wayneleo.quickstart.framework.base.BaseInterface;
 
+@SuppressWarnings( "rawtypes" )
 public interface ConfigCenterService extends BaseInterface {
-    public void save();
+    public void save( ProjectEntity project );
+
+    public void delete( ProjectEntity project );
+
+    public List<ProjectEntity> fetch();
+
+    public List<EnvironmentEntity> fetch( ProjectEntity project );
+
+    public List<ModuleEntity> fetch( EnvironmentEntity environment );
+
+    public Map<KeyEntity, String> fetch( ModuleEntity module );
+
+    public List<ProjectEntity> searchProject( String name );
+
+    public List<EnvironmentEntity> searchEnvironment( String name );
+
+    public List<ModuleEntity> searchModule( String name );
+
+    public Map<KeyEntity, String> searchKey( String name );
 }
